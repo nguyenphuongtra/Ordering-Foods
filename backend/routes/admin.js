807128caa0434b76068b91e83aaef26837564549
Nguyễn Phương Trà <nguyenthanhtra.240805@gmail.com>
2025-07-10 22:27:56 +0700

    Hoàn thiện API Admin quản lý foods, user, orders

---
+++ backend/routes/admin.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const adminMiddleware = require("../middleware/admin");
+const foodController = require("../controllers/foodController");
+const orderController = require("../controllers/orderController");
+const userController = require("../controllers/userController");
+
+router.get("/foods", adminMiddleware, foodController.getFoods);
+router.post("/foods", adminMiddleware, foodController.createFood);
+router.put("/foods/:id", adminMiddleware, foodController.updateFood);
+router.delete("/foods/:id", adminMiddleware, foodController.deleteFood);
+
+router.get("/orders", adminMiddleware, orderController.getOrders);
+router.get("/orders/:id", adminMiddleware, orderController.getOrder);
+router.put("/orders/:id", adminMiddleware, orderController.updateOrder);
+router.delete("/orders/:id", adminMiddleware, orderController.deleteOrder);
+
+router.get("/users", adminMiddleware, userController.getUsers);
+router.get("/users/:id", adminMiddleware, userController.getUserById);
+router.post("/users", adminMiddleware, userController.createUser);
+router.put("/users/:id", adminMiddleware, userController.updateUser);
+router.delete("/users/:id", adminMiddleware, userController.deleteUser);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const express = require("express")
-const cors = require("cors")
-const connectDB = require("./config/database")
-const app = express()
 const session = require('express-session');
+const connectDB = require("./config/database")
+const cors = require("cors")
+const app = express()
 const port = 4000;
 
 const authRouter =  require("./routes/auth")
 const userRouter = require("./routes/user");
 const foodRouter = require("./routes/food");
 const categoryRouter = require("./routes/category");
-
+const adminRouter = require("./routes/admin");
+// const tableRouter = require("./routes/table");
 app.use(express.json())
 app.use(cors())
 app.use(session({
@@ -26,7 +27,8 @@ app.use("/api/user",userRouter)
 app.use("/api/food", foodRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/category", categoryRouter);
-
+// app.use("api/table", tableRouter );
+app.use("/admin", adminRouter);
 app.get("/",(req,res)=>{
     res.send("Hello Team 1")
 })
