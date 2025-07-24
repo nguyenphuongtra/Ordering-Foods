38688aa9d4afb7a7d7d76200c31db7acff1650e9
Nguyễn Phương Trà <nguyenthanhtra.240805@gmail.com>
2025-07-24 11:36:24 +0700

    Fix deploy backend

--- backend/config/google.js
+++ backend/config/google.js
@@ -1,29 +1,43 @@
-module.exports = {
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:4000/api/auth/google/callback"
-};
-
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const googleConfig = require("../config/google");
 const User = require("../models/User");
+
+const googleConfig = {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:4000/api/auth/google/callback"
+};
 
 passport.use(new GoogleStrategy(
   googleConfig,
-  async function(accessToken, refreshToken, profile, done) {
+  async (accessToken, refreshToken, profile, done) => {
     try {
-      const email = profile.emails && profile.emails[0] && profile.emails[0].value;
-      let user = await User.findOne({ googleId: profile.id });
-      if (!user) {
-        user = await User.create({
-          googleId: profile.id,
-          email: email,
-          name: profile.displayName,
-          avatar: profile.photos && profile.photos[0] && profile.photos[0].value
-        });
+      const email = profile.emails?.[0]?.value;
+      if (!email) {
+        return done(new Error("Không thể lấy email từ Google."), null);
       }
-      return done(null, user);
+      let user = await User.findOne({ googleId: profile.id });
+      if (user) {
+        return done(null, user);
+      }
+      user = await User.findOne({ email: email });
+
+      if (user) {
+        user.googleId = profile.id;
+        user.avatar = user.avatar || profile.photos?.[0]?.value;
+        await user.save();
+        return done(null, user);
+      }
+      const newUser = await User.create({
+        googleId: profile.id,
+        email: email,
+        name: profile.displayName,
+        avatar: profile.photos?.[0]?.value,
+        isVerified: true 
+      });
+      
+      return done(null, newUser);
+
     } catch (err) {
       return done(err, null);
     }
@@ -31,10 +45,16 @@ passport.use(new GoogleStrategy(
 ));
 
 passport.serializeUser((user, done) => {
-  done(null, user);
+  done(null, user.id);
 });
-passport.deserializeUser((user, done) => {
-  done(null, user);
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
 });
 
 module.exports = passport;
